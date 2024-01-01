9c300793de2eccd38e4bd0e80a14ffeb9bde25d9
bimal <mutainsbimal@gmail.com>
2024-01-01 09:35:51 +0545

    FreeCodeCamPRatice

---
+++ MS-FCC/CallMethodFromthe.NETClassLibraryUsingC#/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+ public class Program
+  {
+ 
+    public static void Main(string[] args)
+    {
+   
+    PrintHello.myMethod();    
+    }
+  }
+
+  public class PrintHello{
+     public static void myMethod(){
+        Console.WriteLine("Hello World!");
+    }
+  }
+ 

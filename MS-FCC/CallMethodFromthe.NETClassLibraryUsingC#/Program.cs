bc28b7580d013548e614043086fa3db09e8e38a2
bimal <mutainsbimal@gmail.com>
2024-01-01 09:54:29 +0545

    comit

---
+++ MS-FCC/CallMethodFromthe.NETClassLibraryUsingC#/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+ public class Program
+  {
+ 
+    public static void Main(string[] args)
+    {
+   
+    PrintHello helloInstance = new PrintHello();
+        helloInstance.myMethod();    
+    }
+  }
+
+  public class PrintHello{
+     public static void myMethod(){
+        Console.WriteLine("Hello Worldss");
+    }
+  }
+ 
